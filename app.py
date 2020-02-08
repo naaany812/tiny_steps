fd23cf48c46e8773e6afc69dfb6b430be3206240
Annet <naaany812@gmail.com>
2020-02-08 16:40:19 +0300

    fix review marks

--- app.py
+++ app.py
@@ -1,58 +1,61 @@
 from flask import Flask, render_template, request
-from json import loads, dumps
+import json
 from random import sample
 
 app = Flask(__name__)
 
-goals_mock = dict()
-with open(r"data/goals.json", "r", encoding="utf8") as f:
-    content = f.read()
-    goals_mock = loads(content)
+
+def loads_json(path):
+    with open(path, "r", encoding="utf8") as f:
+        content = f.read()
+        return json.loads(content)
 
 
-teachers = []
-with open(r"data/teachers.json", "r", encoding="utf8") as f:
-    contents = f.read()
-    teachers = loads(contents).get("teachers")
+def dumps_json(path, form):
+    with open(path, "a+", encoding="utf8") as f:
+        dumped_data = json.dumps(form)
+        f.write(",\n")
+        f.write(dumped_data)
 
 
-days = dict()
-with open(r"data/days.json", "r", encoding="utf8") as f:
-    content = f.read()
-    days = loads(content)
+goals_mock = loads_json(r"./data/goals.json")
+teachers = loads_json(r"./data/teachers.json").get("teachers")
+days = loads_json(r"./data/days.json")
 
 
 @app.route("/")
 def main():
     random_teachers = sample(teachers, k=6)
-    return render_template("index.html",
-    teachers=random_teachers)
+    return render_template("index.html", teachers=random_teachers)
 
 
 @app.route("/goals/<goal>/")
 def goals(goal):
     goal_value = goals_mock.get(goal)
     needed_teachers = list()
+    t_params = ["id", "name", "about", "rating", "picture", "price", "goals"]
     for teacher in teachers:
         if goal in teacher.get("goals"):
             temp_teacher = dict()
-            for t_id in ["id", "name", "about", "rating", "picture", "price", "goals"]:
+            for t_id in t_params:
                 temp_teacher[t_id] = teacher[t_id]
             needed_teachers.append(temp_teacher)
-    return render_template("goal.html", 
-    goal_value=goal_value,
-    teachers=needed_teachers)
+    return render_template(
+        "goal.html",
+        goal_value=goal_value,
+        teachers=needed_teachers)
 
 
 @app.route("/profiles/<int:teacher_id>/")
 def profile(teacher_id):
     teacher = teachers[teacher_id]
     timesheet = teacher["free"]
-    teacher_goals=", ".join(goals_mock.get(g) for g in teacher.get("goals"))
-    return render_template("profile.html",
-    teacher = teacher,
-    goals = teacher_goals,
-    timesheet = timesheet)
+    teacher_goals = ", ".join(goals_mock.get(g) for g in teacher.get("goals"))
+    return render_template(
+        "profile.html",
+        teacher=teacher,
+        goals=teacher_goals,
+        timesheet=timesheet)
 
 
 @app.route("/request/")
@@ -64,36 +67,30 @@ def send_request():
 def request_done():
     request_form = request.form
     goal = goals_mock.get(request_form.get("goal"))
-    with open(r"data/request.json", "a+", encoding="utf8") as f:
-        request_dumped = dumps(request_form)
-        f.write(",\n")
-        f.write(request_dumped)    
-    return render_template("request_done.html",
-    request=request_form,
-    goal=goal)
+    dumps_json(r"./data/request.json", request_form)
+    return render_template(
+        "request_done.html",
+        request=request_form,
+        goal=goal)
 
 
 @app.route("/booking/<int:teacher_id>/<day>/<time>/")
 def booking(teacher_id, day, time):
     teacher = teachers[teacher_id]
-    selected_day=days.get(day)
-    return render_template("booking.html",
-    teacher=teacher,
-    day=selected_day,
-    time=time)
+    selected_day = days.get(day)
+    return render_template(
+        "booking.html",
+        teacher=teacher,
+        day=selected_day,
+        time=time)
 
 
 @app.route("/booking_done/", methods=["POST"])
 def booking_done():
     booking_info = request.form
-    with open(r"data/booking.json", "a+", encoding="utf8") as f:
-        booking_dumped = dumps(booking_info)
-        f.write(",\n")
-        f.write(booking_dumped)
-    return render_template("booking_done.html",
-    info=booking_info)
+    dumps_json(r"./data/booking.json", booking_info)
+    return render_template("booking_done.html", info=booking_info)
 
 
 if __name__ == '__main__':
     app.run()
-
